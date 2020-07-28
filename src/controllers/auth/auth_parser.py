9dd0c9e22e5908a35973e5a870341a0d2b5d4afe
Diodo Dev <djdiodo@gmail.com>
2020-07-28 17:46:37 -0300

    feat(authentication): Hot swagger.json

--- src/controllers/auth/auth_parser.py
+++
@@ -1,5 +0,0 @@
-from flask_restplus import reqparse
-
-parser = reqparse.RequestParser()
-parser.add_argument('username', help='This field cannot be blank', required=True)
-parser.add_argument('password', help='This field cannot be blank', required=True)
